8673012d4a5d85a63eea8e1f897b1c3915c7ff30
ByteAssembler <65082118+ByteAssembler@users.noreply.github.com>
2024-12-14 21:07:23 +0100

    chore: Admin page programmed, add function still missing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,20 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  site: "https://example.com",
-  integrations: [sitemap(), tailwind()],
-  server: {
-    port: 3000
-  }
+	output: "server",
+	site: "https://example.com",
+	integrations: [
+		sitemap(),
+		tailwind({
+			// applyBaseStyles: false,
+		}),
+		react()
+	],
+	server: {
+		port: 3000
+	}
 });
