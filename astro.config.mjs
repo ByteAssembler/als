043517da198296d804b21ecd96100243a1ff74c5
ByteAssembler <65082118+ByteAssembler@users.noreply.github.com>
2025-02-03 21:48:22 +0100

    chore: Completely new revised project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
-import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-	output: "server",
-	site: "https://example.com",
-	integrations: [
-		sitemap(),
-		tailwind({
-			// applyBaseStyles: false,
-		}),
-		react()
-	],
-	server: {
-		port: 3000
-	}
+	integrations: [tailwind()],
+	output: "static",
 });
---
+++ src/data/navigation.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+	{ text: "Home", href: "#" },
+	{ text: "Events", href: "#events" },
+	{ text: "Comments", href: "#comments" },
+	{ text: "Map", href: "#map" },
+	{ text: "About us", href: "#about" },
+	{ text: "Contact", href: "#contact" },
+] as const;
