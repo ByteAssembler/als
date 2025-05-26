72d17bf89e8a6b7423f8b22bc18743ce4b1299be
ByteAssembler <65082118+ByteAssembler@users.noreply.github.com>
2025-05-26 18:23:41 +0200

    feat: add Svelte integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,26 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
-export default defineConfig({
-	site: "https://smx71k4f-4321.euw.devtunnels.ms",
-	integrations: [tailwind(), react()],
-	output: "server",
-	adapter: netlify(),
+import svelte from "@astrojs/svelte";
 
-	i18n: {
-		locales: ["de", "it", "en"],
-		defaultLocale: "de",
-	},
-	middleware: "./src/middleware.ts",
-	server: {
-		allowedHosts: [
-			"7.tcp.eu.ngrok.io:18524",
-			"7.tcp.eu.ngrok.io",
-			"smx71k4f-4321.euw.devtunnels.ms"
-		]
-	}
+export default defineConfig({
+    site: "https://smx71k4f-4321.euw.devtunnels.ms",
+    integrations: [tailwind(), react(), svelte()],
+    output: "server",
+    adapter: netlify(),
+
+    i18n: {
+        locales: ["de", "it", "en"],
+        defaultLocale: "de",
+    },
+    middleware: "./src/middleware.ts",
+    server: {
+        allowedHosts: [
+            "7.tcp.eu.ngrok.io:18524",
+            "7.tcp.eu.ngrok.io",
+            "smx71k4f-4321.euw.devtunnels.ms"
+        ]
+    }
 });
 
 // import en from './locales/en.json';
